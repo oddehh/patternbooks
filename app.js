a754c747288b322eb0bba99ad4ef263d09949d3b
oddehh <oddehh@gmail.com>
2019-07-28 20:33:06 +0200

    swapped app files to new one

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Book Class: represents a book
 class Book {
   constructor(title, brand, isLend = false, timesLend = 0) {
     this.brand = brand
@@ -6,27 +7,42 @@ class Book {
     this.title = title
     this.isLend = isLend
     this.timesLend = timesLend
-
   }
-
 }
 
+// UI Class: handle UI tasks
 class UI {
+  static displayBooks() {
+    const books = Store.getBooks()
 
-  addBookToList(book) {
-    const list = document.getElementById('book-list')
-    // create tr element
+    books.forEach((book) => UI.addBookToList(book))
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list')
+
     const row = document.createElement('tr')
+    row.classList.add('book-row')
+    row.dataset.bookid = book.id
+
     row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.brand}</td>
       <td>${book.dateCreated}</td>
-      <td><a class="delete" href="#">X</a></td>
+      <td><a class="delete small text-danger" href="#" data-bookId="${book.id}">Usuń</a></td>
     `
     list.appendChild(row)
   }
 
-  showAlert(message, className) {
+  static deleteBook(el) {
+    if(el.classList.contains('delete')) {
+      const bookId = el.dataset.bookid
+      document.querySelector(`.book-row[data-bookId="${bookId}"]`)
+        .remove()
+    }
+  }
+
+  static showAlert(message, className) {
     const div = document.createElement('div')
     div.className = `alert ${className}`
     div.appendChild(document.createTextNode(message))
@@ -36,14 +52,10 @@ class UI {
     // insert alert
     container.insertBefore(div, form)
 
-    setTimeout( () => document.querySelector('.alert').remove(), 3000)
+    setTimeout(() => document.querySelector('.alert').remove(), 3000)
   }
 
-  deleteBook(target) {
-      target.parentElement.parentElement.remove()
-  }
-
-  clearFields() {
+  static clearFields() {
     document.getElementById('title').value = ''
     document.getElementById('brand').value = ''
   }
@@ -55,11 +67,11 @@ class UI {
 }
 
 
+// Store Class: Handles Local Storage
 class Store {
-
   static getBooks() {
     let books
-    if(localStorage.getItem('books') === null) {
+    if (localStorage.getItem('books') === null) {
       books = []
     } else {
       books = JSON.parse(localStorage.getItem('books'))
@@ -70,18 +82,15 @@ class Store {
   static displayBooks() {
     const books = Store.getBooks()
 
-    books.forEach( (book) => {
+    books.forEach((book) => {
       const ui = new UI
-
       ui.addBookToList(book)
     });
   }
 
   static addBook(book) {
     const books = Store.getBooks()
-
     books.push(book)
-
     localStorage.setItem('books', JSON.stringify(books))
   }
 
@@ -89,64 +98,56 @@ class Store {
     const books = Store.getBooks()
 
     books.forEach((book, index) => {
-      if(book.id === titleBrand){
+      if (book.id === titleBrand) {
         books.splice(index, 1)
       }
     })
-
     localStorage.setItem('books', JSON.stringify(books))
   }
-
 }
 
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
-// DOM load event
-document.addEventListener('DOMContentLoaded', Store.displayBooks)
+// Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault()
 
-// event listeners
-document.getElementById('book-form')
-  .addEventListener('submit', function (e) {
-    // get form values
-    let title = document.getElementById('title').value
-    let brand = document.getElementById('brand').value
+  const title = document.querySelector('#title').value;
+  const brand = document.querySelector('#brand').value;
 
-    // instantiate book
-    const book = new Book(title, brand)
 
-    // instantiate UI
-    const ui = new UI()
+  // Validate
+  if(title === '' || brand === '') {
+    UI.showAlert('Uzupełnij tytuł i markę', 'alert-danger')
+  } else {
+    // instatiate book
+    const book = new Book (title, brand)
 
-    // validate
-    if (book.title === '' || book.brand === '') {
-      ui.showAlert('Uzupełnij pola z tytułem i marką', 'alert-danger')
-    } else {
-      // add book to list
-      ui.addBookToList(book)
+    // add book to UI
+    UI.addBookToList(book)
 
-      // add to LS
-      Store.addBook(book)
+    // add to Local Storage
+    Store.addBook(book)
 
-      // show alert
-      ui.showAlert('Wzornik dodany', 'alert-success')
-      // clear fields
-      ui.clearFields()
+    // show success alert
+    UI.showAlert('Wzornik został dodany', 'alert-success')
 
-    }
+    UI.clearFields()
 
-    e.preventDefault()
-  })
+  }
+})
 
-// event listener for delete
-
-document.getElementById('book-list').addEventListener('click', function (e) {
-
-  const ui = new UI()
+// Event remove a book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  e.preventDefault()
+  // remove book from UI
+  UI.deleteBook(e.target)
 
   if (e.target.className === 'delete') {
-    ui.deleteBook(e.target)
-    ui.showAlert('Wzornik usunięty', 'alert-success')
-    let targetId = (e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent + e.target.parentElement.previousElementSibling.previousElementSibling.textContent).replace(/\s+/g, '')
-    Store.removeBook(targetId)
+  // remove book from LS
+  Store.removeBook(e.target.dataset.bookid)
+
+  UI.showAlert('wzornik uzunięty z bazy', 'alert-success')
   }
-  e.preventDefault
 })
---
+++ appbrf.js
@@ -0,0 +1,152 @@
+class Book {
+  constructor(title, brand, isLend = false, timesLend = 0) {
+    this.brand = brand
+    this.dateCreated = UI.dateNow()
+    this.id = (title + brand).replace(/\s+/g, '')
+    this.title = title
+    this.isLend = isLend
+    this.timesLend = timesLend
+
+  }
+
+}
+
+class UI {
+
+  addBookToList(book) {
+    const list = document.getElementById('book-list')
+    // create tr element
+    const row = document.createElement('tr')
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.brand}</td>
+      <td>${book.dateCreated}</td>
+      <td><a class="delete" href="#">X</a></td>
+    `
+    list.appendChild(row)
+  }
+
+  showAlert(message, className) {
+    const div = document.createElement('div')
+    div.className = `alert ${className}`
+    div.appendChild(document.createTextNode(message))
+    // get parent
+    const container = document.querySelector('.container')
+    const form = document.getElementById('book-form')
+    // insert alert
+    container.insertBefore(div, form)
+
+    setTimeout( () => document.querySelector('.alert').remove(), 3000)
+  }
+
+  deleteBook(target) {
+      target.parentElement.parentElement.remove()
+  }
+
+  clearFields() {
+    document.getElementById('title').value = ''
+    document.getElementById('brand').value = ''
+  }
+
+  static dateNow() {
+    let utc = new Date().toJSON().slice(0, 10).replace(/-/g, '/')
+    return utc
+  }
+}
+
+
+class Store {
+
+  static getBooks() {
+    let books
+    if(localStorage.getItem('books') === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem('books'))
+    }
+    return books
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks()
+
+    books.forEach( (book) => {
+      const ui = new UI
+
+      ui.addBookToList(book)
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks()
+
+    books.push(book)
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook(titleBrand) {
+    const books = Store.getBooks()
+
+    books.forEach((book, index) => {
+      if(book.id === titleBrand){
+        books.splice(index, 1)
+      }
+    })
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+}
+
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', Store.displayBooks)
+
+// event listeners
+document.getElementById('book-form')
+  .addEventListener('submit', function (e) {
+    // get form values
+    let title = document.getElementById('title').value
+    let brand = document.getElementById('brand').value
+
+    // instantiate book
+    const book = new Book(title, brand)
+
+    // instantiate UI
+    const ui = new UI()
+
+    // validate
+    if (book.title === '' || book.brand === '') {
+      ui.showAlert('Uzupełnij pola z tytułem i marką', 'alert-danger')
+    } else {
+      // add book to list
+      ui.addBookToList(book)
+
+      // add to LS
+      Store.addBook(book)
+
+      // show alert
+      ui.showAlert('Wzornik dodany', 'alert-success')
+      // clear fields
+      ui.clearFields()
+
+    }
+
+    e.preventDefault()
+  })
+
+// event listener for delete
+
+document.getElementById('book-list').addEventListener('click', function (e) {
+
+  const ui = new UI()
+
+  if (e.target.className === 'delete') {
+    ui.deleteBook(e.target)
+    ui.showAlert('Wzornik usunięty', 'alert-success')
+    let targetId = (e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent + e.target.parentElement.previousElementSibling.previousElementSibling.textContent).replace(/\s+/g, '')
+    Store.removeBook(targetId)
+  }
+  e.preventDefault
+})
